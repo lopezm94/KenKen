956a7ace89bb0092558c631f0a6727057ad90c9e
marc ortiz torres <mrk.22.10@gmail.com>
2015-11-15 00:49:47 +0100

    Pujar Excepcions gestio dades

--- GestioDadesH.java
+++ GestioDadesH.java
@@ -1,5 +1,16 @@
 public class GestioDadesH  extends Gestio_Dades{
 	public GestioDadesH(){
-		
+		super();
+		//Comprovar que existeixen tots els fitxers i directoris
+		//Si no existeixen crear-los
+	}
+	public Boolean existsUser(String username){
+		boolean exists = false;
+	}
+	public getPassword(String username){
+		//Pre: username existeix al fitxer Profiles.txt
+	}
+	public getPuntuacions(String username){
+		//Pre: username exists in Profiles.txt
 	}
 }
