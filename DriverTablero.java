d8b4f5be4337dcdeb0675aadc2e3f1929b3232b3
reyes9 <reyesvera9@gmail.com>
2015-11-15 20:16:54 +0100

    modifi

---
+++ DriverTablero.java
@@ -0,0 +1,123 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+//import java.io.IOException;
+import java.util.Scanner;
+//import java.io.*;
+
+
+/*@version 1.0
+*@author Reyes Vera
+*/
+
+
+public class DriverTablero {
+
+
+	public static void main(String[] args) {
+		Scanner input = null;
+		File test = null;
+
+		try {
+			test = new File("test3");
+			System.out.println("Entra tamaño KenKen:");
+			input = new Scanner(test);
+			int var = input.nextInt();
+			int casellas;
+			TableroH tablero = new TableroH(var);
+			casellas = tablero.files * tablero.files;
+			for (int i = 0; i < tablero.files;++i){
+				for (int j = 0; j < tablero.files;++j){
+					var = input.nextInt();
+					Boolean var2 = input.nextBoolean();
+					int var3 = input.nextInt();
+					Casilla cas = new Casilla(var,var2,var3);
+					tablero.setCasilla(cas,i,j);
+					if (var2) --casellas;
+				}
+			}
+			var = input.nextInt();
+			while (var != -1){
+				System.out.println("Quieres tener un area de tipo? (+,-,*,/)");
+<<<<<<< HEAD
+=======
+				System.out.println("Quieres tener un ï¿½rea de tipo? (+,-,*,/)");
+>>>>>>> 5bd503a30e6c0dbe52058502aec09ee756f2ed30
+				String varS = input.next();
+				char var2[] = varS.toCharArray();
+				Area a = new Area(var,var2[0]);
+	/*---->*/			tablero.afegirArea(a,0);
+				var = input.nextInt();
+			}
+			int a = tablero.files * tablero.files;
+			System.out.println("Que area le quieres poner a cada casilla?");
+			while(a != 0){
+				var = input.nextInt();
+				int var2 = input.nextInt();
+				int var3 = input.nextInt();
+				tablero.setid(var,var2,var3);
+				--a;
+			}
+			Boolean acabat = false;
+			while (! acabat){
+				int varC = casellas;
+				while(varC != 0){
+					var = input.nextInt();
+					int var2 = input.nextInt();
+					int var3 = input.nextInt();
+					tablero.setCasillaVal(var,var2,var3);
+					--varC;
+				}
+
+				System.out.println(tablero);
+
+				Boolean resolv = false;
+				if (tablero.tableroCheck()){
+					if (tablero.numerosCheck()){
+						System.out.println("El KenKen es correcto, FELICIDADES!");
+						acabat = true;
+					}
+					else{
+						System.out.println("Tienes problemas en la colocaciï¿½n de los numeros");
+						resolv = true;
+					}
+				}
+				else{
+					System.out.println("Tienes problemas con las areas, vigila tambien con los numeros!");
+					resolv = true;
+				}
+				if (resolv){
+					Boolean ok = false;
+					while (! ok){
+						System.out.println("Quieres ver la soluciï¿½n del KenKen? (Si/No)");
+						String varS = input.next();
+						char var2[] = varS.toCharArray();
+						if (var2[0] == 'S' && var2[1] == 'i'){
+							String res = "";
+							for (int i = 0; i < tablero.files;++i){
+								for (int j = 0; j < tablero.files; ++j){
+									res = res + tablero.getCasillaSol(i,j) + " ";
+								}
+							res = res + "\n";
+							}
+							System.out.println(res);
+							ok = true;
+							acabat = true;
+						}
+						else if (var2[0] == 'N' && var2[1] == 'o') ok = true;
+						else System.out.println("Como?");
+						}
+				 }
+			}
+	        } catch (FileNotFoundException e) {
+	          e.printStackTrace();
+	        } finally {
+	          if (input != null) {
+	            input.close();                      // Close the file scanner.
+	        }
+	    }
+
+
+
+	}
+
+}
---
+++ KenkenSolver.java
@@ -0,0 +1,89 @@
+import java.io.IOException;
+import java.util.LinkedList;
+
+/*@version 1.0ermino
+*@author Reyes Vera
+*/
+
+
+public class KenkenSolver {
+
+	private TableroH tablero;
+
+	public KenkenSolver(TableroH tablero){
+		this.tablero = tablero;
+		this.initDomain();
+	}
+
+	private Boolean FuncionRecursiva(int x, int y, ConstraintEngine ce) {
+		if (y == this.tablero.size()) {
+			//System.out.println("termino");
+			ce.storeSolution();
+			//System.out.println(tablero);
+
+		/*	try{
+				System.in.read();
+			}
+			catch(IOException e){
+				
+			}*/
+			return true;
+			
+		}
+		Boolean check = false;
+		Boolean done = false;
+		int newx = (x+1)%this.tablero.size();
+		int newy = y;
+		if (newx == 0) newy++;
+
+		for (Integer value : new LinkedList<Integer>(ce.getDomain(x,y))) {
+			//System.out.println("hola");
+			check = ce.propagate(x, y, value);
+			//System.out.println(ce);
+			//System.out.println("x: " + x + " y: " + y + " value: " + value + " entra");
+
+			/*try{                       
+				System.in.read();      
+			}                          
+			catch(IOException e){      
+				                       
+			}  */                        
+			if (check){
+				//System.out.println("x: " + x + " y: " + y + " value: " + value + " entra");
+				done = this.FuncionRecursiva(newx,newy,ce);
+			}
+			else 
+				//System.out.println("no entra");
+			ce.depropagate(x,y);
+			//System.out.println(ce);
+
+			/*try{                       
+				System.in.read();      
+			}                          
+			catch(IOException e){      
+				                       
+            } */                         
+			if (done) break;
+		}
+		return done;
+	}
+
+	public Boolean solveKenken(){
+		ConstraintEngine ce = new ConstraintEngine(tablero);
+		return this.FuncionRecursiva(0,0,ce);
+	}
+
+	public void initDomain(){
+		for (int i = 0; i < tablero.size(); ++i){
+			for (int j = 0; j < tablero.size();++j){
+				for (int k = 1; k <= tablero.size();++k){
+					tablero.getCasilla(i,j).addCan(k);
+				}
+			}
+		}
+	//	System.out.println(tablero);
+
+	}
+
+
+}
